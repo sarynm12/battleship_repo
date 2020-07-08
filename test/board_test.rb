75bf6644951d5f2806a30c4630ca0d30f6af9c58
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-08 17:46:00 -0600

    board class created with initial tests passing

---
+++ test/board_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+class BoardTest < Minitest::Test
+
+  def test_it_exists
+    board = Board.new
+    assert_instance_of Board, board
+  end
+
+  def test_it_has_cells
+    board = Board.new
+    assert_equal 16, board.cells.keys.count
+    assert_equal 16, board.cells.values.count
+  end
+
+  def test_it_knows_if_coordinate_is_valid
+    board = Board.new
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal false, board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("A22")
+  end
+
+end
