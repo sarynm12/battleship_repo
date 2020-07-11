622fa19c17b79174bec114e033cc408b3a16a3ea
Saryn Mooney <smooney15@my.whitworth.edu>
2020-07-11 09:11:25 -0600

    overlap ships method passing

--- test/board_test.rb
+++ test/board_test.rb
@@ -67,5 +67,13 @@ class BoardTest < Minitest::Test
     assert_equal true, cell_3.ship == cell_2.ship
   end
 
+  def test_it_wont_let_ships_overlap
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    board.place(cruiser, ["A1", "A2", "A3"])
+    submarine = Ship.new("Submarine", 2)
+    assert_equal false, board.valid_placement?(submarine, ["A1", "B1"])
+  end
+
 
 end
